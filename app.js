34dcf8db7d63316485361db3550e5924426d6d3b
李奇涵 <grove@liqihandeMacBook-Pro.local>
2017-07-29 14:18:35 +0800

    change config

--- app.js
+++ app.js
@@ -5,29 +5,33 @@ var exec = require('child_process').exec;
 var bodyParser = require('body-parser');
 var util = require('util');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.post('/', function (req, res) {
     const ref = req.body.ref;
-    if (ref !== 'refs/heads/master') {
-        res.send('not master');
-        return;
-    }
     const project = JSON.parse(req.body.project);
     const name = project.name;
     const gitRepo = project.git_ssh_url;
-    exec(util.format('./pre.sh %s %s', name, gitRepo), function (err) {
+    var pre = exec(util.format('./pre.sh %s %s %s', name, gitRepo, ref), function (err, stdout, stderr) {
         if (err) {
             res.send('error');
             return;
         }
         res.send('success');
         return;
-    })
+    });
+    pre.stdout.on('data', chunk => {
+        cosole.log(chunk);
+    });
+    pre.stderr.on('data', chunk => {
+        console.log('err:', chunk);
+    });
 })
 
 var server = app.listen(9899, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+    var host = server.address().address;
+    var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+    console.log('Example app listening at http://%s:%s', host, port);
 });
--- pre.sh
+++ pre.sh
@@ -2,16 +2,19 @@
 
 # path='/home/admin/pre-server'
 path="/Users/grove/Grove/personal/test"
+set -x
 cd ${path}
 [ "$#" -lt 2 ] && echo "The number of parameter is less than 2" && exit 0
 name=$1;
 repo=$2;
+ref=$3;
+echo "start "
 rm -rf ${name}
 git clone ${repo}
 if [[ $? -ne 0 ]];then
     exit 1
 fi
-cd ${name} && npm install
+cd ${name} && git checkout $ref &&  npm install
 if [[ $? -ne 0 ]];then
     exit 1
 fi
